414d9b38432ad15fc72887e5cefd74793a11ed1f
jose <elcharlie@gmail.com>
2020-05-22 00:07:56 -0400

    primer ejemplo funcional

--- control1.py
+++ control1.py
@@ -1,64 +1,138 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 '''
 # Script para el control 1 de redes,
 por José Espina (joseguillermoespina@gmail.com)
  
 ## Modo de uso:
- * Primero, ejecutar el servidor. Fijarse que el archivo a descargar exista
-./control1 servidor
- * Ejecutar netem para simular pérdida de paquetes
- <codigo netem aquii>
- * Ejecutar el cliente
-./control1 cliente
+ * Ejecutar netem como superusuario para simular pérdida de paquetes y retraso en la red
+ tc qdisc add dev lo root netem loss 20.0% delay 0.5s
+ * Ejecutar el servidor. Fijarse que el archivo a descargar exista
+ ./control1 servidor ""
+ * Hacer una solitud http GET con curl en la dirección que aparece por stdout en el paso anterior
+ ejemplo: curl 127.0.0.1:9999
 '''
 import argparse
-import logging
 import socket
+import logging
 import sys
+import socketserver
+import threading
 
-# El servidor envía un archivo "hardcoded"
-def servidor(puerto) :
-    filename='control1.pdf'
-    print("Sending:", filename)
-    with open(filename, 'rb') as f:
-        raw = f.read()
-    # Send actual length ahead of data, with fixed byteorder and size
-    self.sock.sendall(len(raw).to_bytes(8, 'big'))
-    # You have the whole thing in memory anyway; don't bother chunking
-    self.sock.sendall(raw)
+logging.basicConfig(level=logging.DEBUG)
 
-# El ciente recibe el archivo en fragmentos
-def cliente(puerto) :
-    # Get the expected length (eight bytes long, always)
-    expected_size = b""
-    while len(expected_size) < 8:
-        more_size = conn.recv(8 - len(expected_size))
-        if not more_size:
-            raise Exception("Short file length received")
-        expected_size += more_size
+class EchoRequestHandler(socketserver.BaseRequestHandler):
 
-    # Convert to int, the expected file length
-    expected_size = int.from_bytes(expected_size, 'big')
+    def __init__(self, request, client_address, server):
+        self.logger = logging.getLogger('EchoRequestHandler')
+        self.logger.debug('__init__')
+        socketserver.BaseRequestHandler.__init__(self, request,client_address,server)
+        return
 
-    # Until we've received the expected amount of data, keep receiving
-    packet = b""  # Use bytes, not str, to accumulate
-    while len(packet) < expected_size:
-        buffer = conn.recv(expected_size - len(packet))
-        if not buffer:
-            raise Exception("Incomplete file received")
-        packet += buffer
-    with open(filename, 'wb') as f:
-        f.write(packet)
+    def setup(self):
+        self.logger.debug('setup')
+        return socketserver.BaseRequestHandler.setup(self)
+
+    def handle(self):
+        self.logger.debug('handle')
+        print(self.request.getpeername())
+        # Echo the back to the client
+        data = self.request.recv(128)
+        str_data = data.decode('utf-8')
+        print(str_data)
+        print(str_data.startswith('GET /'))
+        self.logger.debug('recv()->"%s"', data)
+        self.request.send(data)
+        return
+
+    def finish(self):
+        self.logger.debug('finish')
+        return socketserver.BaseRequestHandler.finish(self)
     
-if __name__ == '__main__' :
-    if sys.version_info.major !=3 :
-        print("Este script funciona con python 3 o superior")
-        sys.exit(-1)
-    logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.DEBUG)
-    choices = {'cliente': cliente, 'servidor':servidor}
-    parser = argparse.ArgumentParser(description='Script para el control 1, por José Espina (joseguillermoespina@gmail.com)')
-    parser.add_argument('role', choices=choices,help='Rol del script')
-    parser.add_argument('-p',metavar='PORT', type=int, default=1060,help='Puerto (default 1060)')
-    args=parser.parse_args()
-    function=choices[args.role]
-    function(args.p)
+class EchoServer(socketserver.TCPServer):
+
+    def __init__(self, server_address, handler_class=EchoRequestHandler,):
+        self.logger = logging.getLogger('EchoServer')
+        self.logger.debug('__init__')
+        socketserver.TCPServer.__init__(self, server_address, handler_class)
+        return
+
+    def server_activate(self):
+        self.logger.debug('server_activate')
+        socketserver.TCPServer.server_activate(self)
+        return
+
+    def serve_forever(self, poll_interval=0.5):
+        self.logger.debug('waiting for request')
+        self.logger.info(
+            'Handling requests, press <Ctrl-C> to quit'
+        )
+        socketserver.TCPServer.serve_forever(self, poll_interval)
+        return
+
+    def handle_request(self):
+        self.logger.debug('handle_request')
+        return socketserver.TCPServer.handle_request(self)
+
+    def verify_request(self, request, client_address):
+        self.logger.debug('verify_request(%s, %s)',
+                          request, client_address)
+        return socketserver.TCPServer.verify_request(
+            self, request, client_address,
+        )
+
+    def process_request(self, request, client_address):
+        self.logger.debug('process_request(%s, %s)',
+                          request, client_address)
+        return socketserver.TCPServer.process_request(
+            self, request, client_address,
+        )
+
+    def server_close(self):
+        self.logger.debug('server_close')
+        return socketserver.TCPServer.server_close(self)
+
+    def finish_request(self, request, client_address):
+        self.logger.debug('finish_request(%s, %s)',
+                          request, client_address)
+        return socketserver.TCPServer.finish_request(
+            self, request, client_address,
+        )
+
+    def close_request(self, request_address):
+        self.logger.debug('close_request(%s)', request_address)
+        return socketserver.TCPServer.close_request(
+            self, request_address,
+        )
+
+    def shutdown(self):
+        self.logger.debug('shutdown()')
+        return socketserver.TCPServer.shutdown(self)
+
+def server(interface, port):
+    address = ('localhost', 9999)  # let the kernel assign a port
+    server = EchoServer(address, EchoRequestHandler)
+    ip, port = server.server_address  # what port was assigned?
+    print('servidor {ip}:{puerto}'.format(ip=ip, puerto=port))
+    try :
+        server.serve_forever()
+    except KeyboardInterrupt :
+        pass
+    finally :
+        server.shutdown()
+        server.socket.close()
+        print("Gracias!\n")
+
+def client(host, port):
+    pass
+
+if __name__ == '__main__':
+    choices = {'client': client, 'server': server}
+    parser = argparse.ArgumentParser(description='Send and receive over TCP')
+    parser.add_argument('role', choices=choices, help='which role to play')
+    parser.add_argument('host', help='interface the server listens at;'
+                        ' host the client sends to')
+    parser.add_argument('-p', metavar='PORT', type=int, default=1060,
+                        help='TCP port (default 1060)')
+    args = parser.parse_args()
+    function = choices[args.role]
+    function(args.host, args.p)
